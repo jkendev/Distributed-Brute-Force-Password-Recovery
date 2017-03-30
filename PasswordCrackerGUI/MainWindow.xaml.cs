474d322863cf5517fd9ae153ea8963fd96ee5a0c
Justin Kennedy <jkennedydeveloper@gmail.com>
2017-03-31 07:07:26 +0800

    minor comment fix

--- PasswordCrackerGUI/MainWindow.xaml.cs
+++ PasswordCrackerGUI/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace PasswordCrackerGUI
     /// </summary>
     public partial class MainWindow : Window, IRecoveryManagerGUICallback
     {
-        char[] charSet = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };   //now I know my abc
+        char[] charSet = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };   //char set according to assignment specification
         
         private NetTcpBinding tcpBinding;
         private String recovManagerURL = "net.tcp://localhost:5002/RecovManager";   //URL of manager.
